02b806790a7f078670225ce55c7e75dadf059204
Sven Eden <sven.eden@prydeworx.com>
2022-02-27 16:08:59 +0100

    cleanup_headers.sh: remove autotools code and make the searching more strict
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- cleanup_headers.sh
+++ cleanup_headers.sh
@@ -1,38 +1,73 @@
 #!/bin/bash
 
-src_files=( $(find -type f -name '*.c') )
-hdr_files=( $(find -type f -name '*.h') )
-
-# Is this a meson build?
-isMeson=0
-if [[ -f meson.build ]]; then
-	isMeson=1
+do_debug=0
+if [[ "x--debug" = "x$1" ]]; then
+  do_debug=1
 fi
 
-for hdr in $(find -type f -name '*.h') ; do
-	h_dir="$(basename $(dirname $hdr))"
-	h_file="$(basename $hdr .h)"
+hdr_files=( $(find src/ -type f -name '*.h') )
+src_files=( $(find src/ -type f -name '*.c') )
 
-	# Is it listed in the Makefile.am or meson.build?
-	if [[ 1 -eq $isMeson ]]; then
-		if [[ 0 -lt $(grep -c "$h_dir/${h_file}.c" meson.build) ]] || \
-		   [[ 0 -lt $(grep -c "$h_dir/${h_file}.h" meson.build) ]]; then
-			# It is needed.
-			continue 1
-		fi
-	else
-		if [[ 0 -lt $(grep -c "$h_dir/${h_file}.c" Makefile.am) ]] || \
-		   [[ 0 -lt $(grep -c "$h_dir/${h_file}.h" Makefile.am) ]]; then
-			# It is needed.
-			continue 1
-		fi
-	fi
+# Let's get the maximum lengths first, so we can have prettier output
+max_file_len=0
+max_meso_len=0
+max_path_len=0
+h_dir="none"
+
+for hdr in "${hdr_files[@]}" ; do
+  cur_dir="$(dirname $hdr)"
+  cur_len=$(echo -n "$hdr" | wc -c)
+
+  if [[ $cur_len -gt $max_file_len ]]; then
+    max_file_len=$cur_len
+  fi
+
+  # Also check path lengths
+  if [[ $h_dir != $cur_dir ]]; then
+    h_dir=$cur_dir
+    cur_len=$(echo -n "$h_dir" | wc -c)
+    if [[ $cur_len -gt $max_path_len ]]; then
+      max_path_len=$cur_len
+      ## <path>/meson.build = path len + 12
+      max_meso_len=$((cur_len+12))
+    fi
+  fi
+done
+
+
+for hdr in "${hdr_files[@]}" ; do
+	h_dir="$(dirname $hdr)"
+	h_file="$(basename $hdr .h)"
+	m_file="${h_dir}/meson.build"
+
+  if [[ $do_debug -eq 1 ]]; then
+    printf "%-${max_file_len}s : " "$hdr"
+  fi
+
+	# Is it listed in any meson.build?
+	if [[ -f ${m_file} ]]; then
+    if [[ 0 -lt $(grep -c "${h_file}.c" ${m_file}) ]] || \
+       [[ 0 -lt $(grep -c "${h_file}.h" ${m_file}) ]]; then
+      # It is needed.
+      if [[ $do_debug -eq 1 ]]; then
+        printf "Included in %-${max_meso_len}s\n" "$m_file"
+      fi
+      continue 1
+    fi
+  fi
 
 	# Is it included in any source files?
 	for src in "${src_files[@]}" ; do
 		is_inc=$(grep -P '^#include' $src | grep -c "${h_file}.h")
 		if [[ 0 -lt $is_inc ]]; then
 			# it is indeed
+      if [[ $do_debug -eq 1 ]]; then
+      	if [[ -f ${m_file} ]]; then
+          printf "Included in %-${max_file_len}s but not listed in %-${max_meso_len}s\n" "$src" "$m_file"
+        else
+          printf "Included in %-${max_file_len}s\n" "$src"
+        fi
+      fi
 			continue 2
 		fi
 	done
@@ -40,16 +75,30 @@ for hdr in $(find -type f -name '*.h') ; do
 	# Is it included in any header files?
 	for src in "${hdr_files[@]}" ; do
 
-		# If we already removed $src, skip it
-		[ -f "$src" ] || continue 1
+	  # If we already removed the header, or if it is the checked file, continue
+	  if [[ ! -f $src || $src = $hdr ]]; then
+	    continue 1
+    fi
 
 		is_inc=$(grep '#include' $src | grep -c "${h_file}.h")
 		if [[ 0 -lt $is_inc ]]; then
 			# it is indeed
+      if [[ $do_debug -eq 1 ]]; then
+        if [[ -f ${m_file} ]]; then
+          printf "Included in %-${max_file_len}s but not listed in %-${max_meso_len}s\n" "$src" "$m_file"
+        else
+          printf "Included in %-${max_file_len}s\n" "$src"
+        fi
+      fi
 			continue 2
 		fi
 	done
 
 	# As it was not included, remove it.
-	git rm $hdr
+  if [[ $do_debug -eq 1 ]]; then
+  	echo "Nowhere listed, removing...	git rm -f $hdr"
+  else
+    printf "%-${max_file_len}s nowhere listed, removing...\n" "$hdr"
+    git rm -f $hdr
+  fi
 done
